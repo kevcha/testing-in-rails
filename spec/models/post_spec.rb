a69c0aa70cc538b6b59b32047995f482cdfd91c0
Kevin Chavanne <kevin.chavanne@gmail.com>
2020-03-22 15:02:41 +0100

    Add shoulda-matchers + refactor post_spec file

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,19 +1,8 @@
 require 'rails_helper'
 
 describe Post do
-  it 'validates presence of title' do
-    post = Post.new(body: 'foo')
-
-    expect(post.valid?).to eq(false)
-    expect(post.errors[:title].present?).to eq(true)
-  end
-
-  it 'validates presence of body' do
-    post = Post.new(title: 'foo')
-
-    expect(post.valid?).to eq(false)
-    expect(post.errors[:body].present?).to eq(true)
-  end
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
 
   describe '#published!' do
     it 'sets post as published' do
@@ -24,7 +13,7 @@ describe Post do
       post.publish!
 
       # expectations
-      expect(post.published?).to eq(true)
+      expect(post).to be_published
     end
   end
 end
